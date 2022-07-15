a521d726586f78fd2c769f68920fc9286b3a9221
Jhonatas <jhonatasbcc@gmail.com>
2022-07-15 20:05:52 -0300

    Commit initial

---
+++ src/test/java/com/doefacil/doeFacil/DoeFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doefacil.doeFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoeFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
